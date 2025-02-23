c47bdd109edd8732d8485df243334f3c030ccf7a
Mehul Goyal <145589189+Arnidh@users.noreply.github.com>
2025-02-23 22:43:02 +0530

    Delete src directory

--- src/integrations/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-import type { Database } from './types';
-
-const SUPABASE_URL = "https://puoggocvlysbthkhnuzz.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1b2dnb2N2bHlzYnRoa2hudXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyNDg2MjcsImV4cCI6MjA1NTgyNDYyN30.cBZ8EjmSwbXjwjZ_YM8xXY8gII7UE0GEpzMJMRwMz9s";
-
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
